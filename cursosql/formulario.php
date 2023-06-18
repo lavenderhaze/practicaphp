5506364655b7f8533fea138dfbc21b2c146f6487
lavenderhaze <118069971+lavenderhaze@users.noreply.github.com>
2023-06-18 22:32:20 +0200

    Create formulario.php

---
+++ cursosql/formulario.php
@@ -0,0 +1,29 @@
+<?php
+if ($_POST) {
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "cursosql";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "INSERT INTO usuarios (nombre, apellido, email)
+    VALUES ('$nombre', '$apellido', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
